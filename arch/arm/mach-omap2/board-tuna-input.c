517e0544775fe1f79a81ac4692d9aa81e63cf439
Colin Cross <ccross@android.com>
2011-06-01 12:51:46 -0700

    ARM: omap4: tuna: Move TOUCH_EN gpio for post-lunchbox tunas
    Change-Id: I2298332c8889df10f017a8fad913b26e2149d6f6
    Signed-off-by: Colin Cross <ccross@android.com>
    
    Conflicts:
    	arch/arm/mach-omap2/board-tuna-input.c

--- arch/arm/mach-omap2/board-tuna-input.c
+++ arch/arm/mach-omap2/board-tuna-input.c
@@ -25,6 +25,10 @@
 #include "board-tuna.h"
 #include "mux.h"
 
+#define GPIO_TOUCH_EN		19
+#define GPIO_LUNCHBOX_TOUCH_EN	54
+#define GPIO_TOUCH_IRQ		46
+
 static const int tuna_keymap[] = {
 	KEY(1, 1, KEY_VOLUMEDOWN),
 	KEY(2, 1, KEY_VOLUMEUP),
@@ -119,9 +123,15 @@ void __init omap4_tuna_input_init(void)
 	gpio_direction_input(46);
 	omap_mux_init_gpio(46, OMAP_PIN_INPUT_PULLUP);
 
-	gpio_request(54, "tsp_en");
-	gpio_direction_output(54, 1);
-	omap_mux_init_gpio(54, OMAP_PIN_OUTPUT);
+	if (omap4_tuna_final_gpios()) {
+		gpio_request(GPIO_TOUCH_EN, "tsp_en");
+		gpio_direction_output(GPIO_TOUCH_EN, 1);
+		omap_mux_init_gpio(GPIO_TOUCH_EN, OMAP_PIN_OUTPUT);
+	} else {
+		gpio_request(GPIO_LUNCHBOX_TOUCH_EN, "tsp_en");
+		gpio_direction_output(GPIO_LUNCHBOX_TOUCH_EN, 1);
+		omap_mux_init_gpio(GPIO_LUNCHBOX_TOUCH_EN, OMAP_PIN_OUTPUT);
+	}
 
 	i2c_register_board_info(3, tuna_i2c3_boardinfo,
 		ARRAY_SIZE(tuna_i2c3_boardinfo));
