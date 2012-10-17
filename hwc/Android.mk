37bea520f757321939c878b39b16ab2eb9575186
Mike J. Chen <mjchen@google.com>
2012-10-16 19:15:47 -0700

    Merged hwc with TI mainline version
    Change-Id: I8390a3422c6ff8fa4bc4c0e6a11c51a884aba3cb
    Signed-off-by: Mike J. Chen <mjchen@google.com>
    
    Conflicts:
    	hwc/Android.mk

--- hwc/Android.mk
+++ hwc/Android.mk
@@ -10,14 +10,16 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/../vendor/lib/hw
 LOCAL_SHARED_LIBRARIES := liblog libEGL libcutils libutils libhardware libhardware_legacy libz
-LOCAL_SRC_FILES := hwc.c
+LOCAL_SRC_FILES := hwc.c rgz_2d.c
 LOCAL_STATIC_LIBRARIES := libpng
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE := hwcomposer.omap4
-LOCAL_CFLAGS := -DLOG_TAG=\"ti_hwc\" -I$(LOCAL_PATH)/../kernel-headers
-LOCAL_C_INCLUDES += external/libpng external/zlib
+LOCAL_CFLAGS := -DLOG_TAG=\"ti_hwc\"
+LOCAL_C_INCLUDES += \
+	external/libpng external/zlib \
+	$(HARDWARE_TI_OMAP4_BASE)/kernel-headers-ti
 
 ifeq ($(BOARD_USE_SYSFS_VSYNC_NOTIFICATION),true)
 LOCAL_CFLAGS += -DSYSFS_VSYNC_NOTIFICATION
