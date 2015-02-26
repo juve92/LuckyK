5b10b316dae1536c34a670f6b13735ad056faff3
Fenix46 <scarlataemanuele@gmail.com>
2015-02-26 15:06:17 +0100

    Fix build error

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -628,7 +628,6 @@ void putback_lru_page(struct page *page)
 
 	VM_BUG_ON(PageLRU(page));
 if (active)
- SetPageWasActive(page);
 
 
 redo:
@@ -1287,7 +1286,7 @@ static unsigned long clear_active_flags(struct list_head *page_list,
 		if (PageActive(page)) {
 			lru += LRU_ACTIVE;
 			ClearPageActive(page);
-                        SetPageWasActive(page);
+                        
 			nr_active += numpages;
 		}
 		if (count)
@@ -1709,7 +1708,7 @@ static void shrink_active_list(unsigned long nr_pages, struct zone *zone,
 		}
 
 		ClearPageActive(page);	/* we are de-activating */
-                SetPageWasActive(page);
+                
 		list_add(&page->lru, &l_inactive);
 	}
 
