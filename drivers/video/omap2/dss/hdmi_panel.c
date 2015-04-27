5fa65c6694e09bfa65376a92b2a5a745be346076
Fenix46 <scarlataemanuele@gmail.com>
2015-04-27 19:16:19 +0200

    Fix Build Error
    Signed-off-by: Fenix46 <scarlataemanuele@gmail.com>

--- drivers/video/omap2/dss/hdmi_panel.c
+++ drivers/video/omap2/dss/hdmi_panel.c
@@ -156,16 +156,6 @@ static DEVICE_ATTR(edid, S_IRUGO, hdmi_edid_show, NULL);
 static DEVICE_ATTR(deepcolor, S_IRUGO | S_IWUSR, hdmi_deepcolor_show,
 							hdmi_deepcolor_store);
 
-static ssize_t hdmi_edid_show(struct device *dev,
-		struct device_attribute *attr, char *buf)
-{
-	return omapdss_hdmi_get_edid(buf);
-}
-
-static DEVICE_ATTR(edid, S_IRUGO, hdmi_edid_show, NULL);
-static DEVICE_ATTR(deepcolor, S_IRUGO | S_IWUSR, hdmi_deepcolor_show,
-							hdmi_deepcolor_store);
-
 static struct attribute *hdmi_panel_attrs[] = {
 	&dev_attr_s3d_enable.attr,
 	&dev_attr_s3d_type.attr,
