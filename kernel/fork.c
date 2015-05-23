5e0b22baea3e2656d28027eb5e10c73489f0ae82
Emanuele <scarlataemanuele@gmail.com>
2015-05-23 15:05:34 +0200

    Kernel: fork.c: Fix bug in list_add_tail_rcu
    added a "} else {" because without it, the minimum load of the ram device remained blocked.
     This improves the yield of the Ram, and the power load.
    Signed-off-by: Emanuele Scarlata <scarlataemanuele@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1383,6 +1383,7 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 			list_add_tail(&p->sibling, &p->real_parent->children);
 			list_add_tail_rcu(&p->tasks, &init_task.tasks);
 			__this_cpu_inc(process_counts);
+                } else {
 			list_add_tail_rcu(&p->thread_node,
 					  &p->signal->thread_head);
 		}
