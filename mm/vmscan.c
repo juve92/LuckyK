819f660cab88dc85f1161998db719c8872943606
Emanuele <scarlataemanuele@gmail.com>
2015-06-04 16:42:37 +0200

    Revert "mm: remove vma arg from page_evictable"
    This reverts commit 4225fbdab34e16697d1cf18aee22290b38e0dee2.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -635,7 +635,7 @@ void putback_lru_page(struct page *page)
 redo:
 	ClearPageUnevictable(page);
 
-	if (page_evictable(page)) {
+	if (page_evictable(page, NULL)) {
 		/*
 		 * For evictable pages, we can use the cache.
 		 * In event of a race, worst case is we end up with an
@@ -668,7 +668,7 @@ redo:
 	 * page is on unevictable list, it never be freed. To avoid that,
 	 * check after we added it to the list, again.
 	 */
-	if (lru == LRU_UNEVICTABLE && page_evictable(page)) {
+	if (lru == LRU_UNEVICTABLE && page_evictable(page, NULL)) {
 		if (!isolate_lru_page(page)) {
 			put_page(page);
 			goto redo;
@@ -805,7 +805,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 
 		sc->nr_scanned++;
 
-		if (unlikely(!page_evictable(page)))
+		if (unlikely(!page_evictable(page, NULL)))
 			goto cull_mlocked;
 
 		if (!sc->may_unmap && page_mapped(page))
@@ -1362,7 +1362,7 @@ putback_lru_pages(struct zone *zone, struct scan_control *sc,
 		page = lru_to_page(page_list);
 		VM_BUG_ON(PageLRU(page));
 		list_del(&page->lru);
-		if (unlikely(!page_evictable(page))) {
+		if (unlikely(!page_evictable(page, NULL))) {
 			spin_unlock_irq(&zone->lru_lock);
 			putback_lru_page(page);
 			spin_lock_irq(&zone->lru_lock);
@@ -1655,7 +1655,7 @@ static void shrink_active_list(unsigned long nr_pages, struct zone *zone,
 		page = lru_to_page(&l_hold);
 		list_del(&page->lru);
 
-		if (unlikely(!page_evictable(page))) {
+		if (unlikely(!page_evictable(page, NULL))) {
 			putback_lru_page(page);
 			continue;
 		}
@@ -3364,18 +3364,27 @@ int zone_reclaim(struct zone *zone, gfp_t gfp_mask, unsigned int order)
 /*
  * page_evictable - test whether a page is evictable
  * @page: the page to test
+ * @vma: the VMA in which the page is or will be mapped, may be NULL
  *
  * Test whether page is evictable--i.e., should be placed on active/inactive
- * lists vs unevictable list.
+ * lists vs unevictable list.  The vma argument is !NULL when called from the
+ * fault path to determine how to instantate a new page.
  *
  * Reasons page might not be evictable:
  * (1) page's mapping marked unevictable
  * (2) page is part of an mlocked VMA
  *
  */
-int page_evictable(struct page *page)
+int page_evictable(struct page *page, struct vm_area_struct *vma)
 {
-	return !mapping_unevictable(page_mapping(page)) && !PageMlocked(page);
+
+	if (mapping_unevictable(page_mapping(page)))
+		return 0;
+
+	if (PageMlocked(page) || (vma && is_mlocked_vma(vma, page)))
+		return 0;
+
+	return 1;
 }
 
 /**
@@ -3440,9 +3449,6 @@ void scan_mapping_unevictable_pages(struct address_space *mapping)
 		int pg_scanned = 0;
 
 		zone = NULL;
-
-		if (page_evictable(page)) {
-			enum lru_list lru = page_lru_base_type(page);
 
 		for (i = 0; i < pagevec_count(&pvec); i++) {
 			struct page *page = pvec.pages[i];
