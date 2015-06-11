72eac65bc0deb4501d53f13cabe2ec3ab9c35b1f
Emanuele <scarlataemanuele@gmail.com>
2015-06-11 15:57:52 +0200

    OOM_KILL: remove deprecated oom_adj remove missing oom_adj from oom_kill.c Signed-off-by: Emanuele Scarlata <scarlataemanuele@gmail.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -417,9 +417,8 @@ static void dump_tasks(const struct mem_cgroup *mem, const nodemask_t *nodemask)
 		pr_info("[%5d] %5d %5d %8lu %8lu %3u     %3d         %5d %s\n",
 			task->pid, task_uid(task), task->tgid,
 			task->mm->total_vm, get_mm_rss(task->mm),
-			task_cpu(task), task->signal->oom_adj,
-			task->signal->oom_score_adj, task->comm);
-		task_unlock(task);
+			task_cpu(task), task->signal->oom_score_adj, task->comm);
+		        task_unlock(task);
 	}
 }
 
