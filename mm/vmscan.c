1884d054b665b0826743a6e2cd4e02607fb3939e
Emanuele <scarlataemanuele@gmail.com>
2015-07-10 13:53:11 +0200

    Revert " staging:zcache: patches and updates"
    This reverts commit 20735fbf304e08d3171e38bc41033a292ea66a13.
    
    Conflicts:
    	mm/cleancache.c
    	mm/vmscan.c

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -627,10 +627,6 @@ void putback_lru_page(struct page *page)
 	int was_unevictable = PageUnevictable(page);
 
 	VM_BUG_ON(PageLRU(page));
-#ifdef CONFIG_CLEANCACHE
-	if (active)
-		SetPageWasActive(page);
-#endif
 
 redo:
 	ClearPageUnevictable(page);
@@ -1254,9 +1250,6 @@ static unsigned long clear_active_flags(struct list_head *page_list,
 		if (PageActive(page)) {
 			lru += LRU_ACTIVE;
 			ClearPageActive(page);
-#ifdef CONFIG_CLEANCACHE
-			SetPageWasActive(page);
-#endif
 			nr_active += numpages;
 		}
 		if (count)
@@ -1678,9 +1671,6 @@ static void shrink_active_list(unsigned long nr_pages, struct zone *zone,
 		}
 
 		ClearPageActive(page);	/* we are de-activating */
-#ifdef CONFIG_CLEANCACHE
-		SetPageWasActive(page);
-#endif
 		list_add(&page->lru, &l_inactive);
 	}
 
